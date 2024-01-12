e49d50445414f3af81f00fd950cbfb4722f5cd57
Yash <yashpatel.199761@gmail.com>
2024-01-11 22:17:01 -0500

    robofriends app with react

--- src/index.js
+++ src/index.js
@@ -1,15 +1,27 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
+import Card from './Card';
 import reportWebVitals from './reportWebVitals';
+import 'tachyons';
+import { robots } from './robots';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+    <div className="tc">
+        <Card id={robots[0].id} name={robots[0].name} email={robots[0].email} />
+        <Card id={robots[1].id} name={robots[1].name} email={robots[1].email} />
+        <Card id={robots[2].id} name={robots[2].name} email={robots[2].email} />
+        <Card id={robots[3].id} name={robots[3].name} email={robots[3].email} />
+        <Card id={robots[4].id} name={robots[4].name} email={robots[4].email} />
+        <Card id={robots[5].id} name={robots[5].name} email={robots[5].email} />
+        <Card id={robots[6].id} name={robots[6].name} email={robots[6].email} />
+        <Card id={robots[7].id} name={robots[7].name} email={robots[7].email} />
+        <Card id={robots[8].id} name={robots[8].name} email={robots[8].email} />
+        <Card id={robots[9].id} name={robots[9].name} email={robots[9].email} />   
+    </div>
 );
+
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
---
+++ src/robots.js
@@ -0,0 +1,62 @@
+export const robots = [
+    {
+      id: 1,
+      name: 'Yyyyy Kkkkkkkk',
+      username: 'YKkkk',
+      email: 'Ykkkk@own.dis'
+    },
+    {
+      id: 2,
+      name: 'check man',
+      username: 'checking',
+      email: 'Check@man.rit'
+    },
+    {
+      id: 3,
+      name: 'Get it',
+      username: 'gottta',
+      email: 'Getit@jet.kal'
+    },
+    {
+      id: 4,
+      name: 'Not me',
+      username: 'fake',
+      email: 'Notme@kal.brt'
+    },
+    {
+      id: 5,
+      name: 'Dav nil',
+      username: 'nilda',
+      email: 'davnil@met.kol'
+    },
+    {
+      id: 6,
+      name: 'Collin Linda',
+      username: 'lello',
+      email: 'collin@asper.vt'
+    },
+    {
+      id: 7,
+      name: 'kullu manali',
+      username: 'thandi',
+      email: 'kullu@man.iy'
+    },
+    {
+      id: 8,
+      name: 'Call out',
+      username: 'poke',
+      email: 'callout@cal.in'
+    },
+    {
+      id: 9,
+      name: 'hello man',
+      username: 'saying',
+      email: 'hello@jat.eak'
+    },
+    {
+      id: 10,
+      name: 'Mela Kumbh',
+      username: 'lost',
+      email: 'kumbh@har.kat'
+    }
+  ];
