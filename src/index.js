1b5e0b0e99d6df4b0094927993b3014f896ee387
Yash <yashpatel.199761@gmail.com>
2024-01-17 21:43:34 -0500

    Creating Main component as App and implementing searchBox alon with state management. Make search box responsive by changing state and using searchfield.

--- src/index.js
+++ src/index.js
@@ -1,16 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import CardList from './CardList';
+import App from './App';
 import reportWebVitals from './reportWebVitals';
 import 'tachyons';
-import { robots } from './robots';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-    <div className="tc">
-        <CardList robots={robots} /> 
-    </div>
+    <App />
 );
 
 
