3d23dc204ceafc1468fa9e9f0694ba2735d20354
Yash <yashpatel.199761@gmail.com>
2024-05-04 19:03:37 -0400

    Udate class for heading RoboFriends

--- src/App.js
+++ src/App.js
@@ -24,7 +24,7 @@ onSearchChange = (event) => {
         })
         return (
             <div className="tc">
-                <h1>RoboFriends</h1>
+                <h1 className="f1">RoboFriends</h1>
                 <SearchBox searchChange={this.onSearchChange} />
                 <CardList robots={filteredRobots} />
             </div>
