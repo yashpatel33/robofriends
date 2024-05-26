6186f30f2b0cd6a6d4549fa9bb78ffd100652956
Yash <yashpatel.199761@gmail.com>
2024-05-25 23:53:41 -0400

    Username in Card

--- src/CardList.js
+++ src/CardList.js
@@ -11,7 +11,8 @@ const CardList = ({ robots}) => {
                         key={i}
                         id={robots[i].id} 
                         name={robots[i].name} 
-                        email={robots[i].email} 
+                        email={robots[i].email}
+                        user={robots[i].username} 
                         /> 
                     );
                 })
